d73c0322512215ac3dbe63245c5718ff537577b2
abdul44haseeb <48325305+abdul44haseeb@users.noreply.github.com>
2023-04-13 14:10:30 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,4 +1,46 @@
 <!DOCTYPE html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+
+    // Validate input fields
+    $name = test_input($name);
+    $email = test_input($email);
+    $phone = test_input($phone);
+    $message = test_input($message);
+
+    // Check if any field is empty
+    if (empty($name) || empty($email) || empty($phone) || empty($message)) {
+        echo "Please fill all fields";
+        exit;
+    }
+
+    // Create email body
+    $to = "contact@mission.thrissur.in";
+    $subject = "New Contact Request";
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message";
+
+    // Send email
+    $headers = "From: $email\r\nReply-To: $email\r\n";
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for contacting us!";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
 <html>
 <head>
 	<title>Contact Form</title>
